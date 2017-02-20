b28641376ea7d8a0e11c770e28579da9dd53fce7
narasimha kbnd <narasimha kbnd>
2017-02-20 10:05:19 +0530

    addedcode

--- server.js
+++ server.js
@@ -3,23 +3,45 @@ var express = require("express");
 
 //execute the express module
 var app = express();
-
-app.get("/*", handleDefaultRoute);
-// app.get("/login", handleLogin);
+app.use(express.static("assets"));
+app.use(doLogging);
+app.get("/", handleDefaultRoute);
+//app.get("/",doLogging, handleDefaultRoute);
+app.get("/login", handleLogin);
+app.get("/products", products);
 //app.get("/",handleDefaultRoute);
 function handleDefaultRoute(req, res) {
-    if (req.url=="/") {
+    if (req.url == "/") {
         res.sendFile(__dirname + "/index.html");
     }
-    else 
-    {
-         res.sendFile(__dirname + req.url+".html");
-    }
+    // else 
+    // {
+    //      res.sendFile(__dirname + req.url+".html");
+    // }
 
 }
 
 function handleLogin(req, res) {
     res.sendFile(__dirname + "/login.html");
+}
+
+function products(req, res) {
+    res.sendFile(__dirname + "/products.html");
+}
+
+function doLogging(req, res, xyz) {
+    if (req.url == "/") {
+        console.log("Hey i am serving index file");
+        xyz();
+    }
+    else if (req.url == "/products") {
+        console.log("User not authenticated");
+        res.sendFile(__dirname + "/login.html");
+    }
+    else {
+        xyz();
+    }
+
 }
 
 app.listen(3000);
