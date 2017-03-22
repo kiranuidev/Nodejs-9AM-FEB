d235a92f8a777a26a3076befc08ae08688f5e425
padma <padmab@k3informatics.com>
2017-03-22 09:58:28 +0530

    added code

---
+++ app/fileUpload/fileUpload.js
@@ -0,0 +1,29 @@
+var multer = require('multer');
+
+
+
+module.exports = function (app) {
+    
+    app.post("/api/upload", function (req, res) {
+       var storage	=	multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './uploads');
+  },
+  filename: function (req, file, callback) {
+    
+    callback(null, file.originalname + '-' + Date.now()+".jpg");
+  }
+});
+var upload = multer({ storage : storage}).single('userPhoto');
+       
+        console.log("file uploading");
+         console.log(req);
+        upload(req, res, function (err) {
+            if (err) {
+                console.log(err);
+                return res.end("Error uploading file.");
+            }
+            res.end("File is uploaded");
+        });
+    });
+}
--- config/logger.js
+++ config/logger.js
@@ -1,4 +1,5 @@
 module.exports = function(req,res,next){
     console.log(`Worker ${process.pid} Request`);
+    console.log(req.url);
   next();
 }
